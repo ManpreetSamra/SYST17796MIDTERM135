7f1c32d479204a024940e208cc6a39d8cde0bc08
gurus <gurus@192.168.2.13>
2021-02-25 13:19:44 -0500

    Committed the following changes 1.add calMax()method.2)add exception etc

--- src/javaapplication11/JavaApplication11.java
+++ src/javaapplication11/JavaApplication11.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package javaapplication11;
-
+import java.util.Scanner;
 /**
  *
  * @author r2kar
@@ -49,7 +49,11 @@ public class JavaApplication11 {
     public static void main(String[] args) {
         // TODO code application logic here
         // try to get the input (myStr) from the user
-        String myStr= "SUMMER";
+        String myStr;
+        System.out.print("ENter the value");
+        Scanner input=new Scanner(System.in);
+        myStr=input.nextLine();
+        System.out.println();
         JavaApplication11 test1= new JavaApplication11(Seasons.valueOf(myStr));
         test1.seasonDescription();
         //test1.isSeason();
@@ -63,6 +67,11 @@ public class JavaApplication11 {
         }
        
         }
+    
+    public boolean isSeason(){
+        boolean status=true;
+        return status;
+    }
     }
     
 
